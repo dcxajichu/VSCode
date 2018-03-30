339c5250cdd9337d3855b10489ca51a3ea8358f2
luocg <306139142@qq.com>
2018-03-30 17:23:25 +0800

    VSCode使用技巧总结
    上传保存

---
+++ 总结/vscode设置.js
@@ -0,0 +1,30 @@
+// 用户设置
+// 将设置放入此文件中以覆盖默认设置
+{
+    "editor.fontSize": 17,
+    "workbench.colorTheme": "Visual Studio Dark",
+    "workbench.iconTheme": "vs-seti",
+    "update.channel": "none",
+    "emmet.syntaxProfiles": {
+        "vue-html": "html",
+        "vue": "html"
+    },
+    "editor.tabSize": 2,
+    "editor.detectIndentation": false,
+    "terminal.integrated.shell.windows": "C:\\Windows\\Sysnative\\cmd.exe"
+}
+
+// 工作区设置
+// 将设置放入此文件中以覆盖默认值和用户设置。
+{
+  "eslint.options": {
+    "configFile": "G:/sceo2.0/project/bizmodel1.0/it10/.eslintrc.js",
+    "plugins": ["html"]
+  },
+  "eslint.validate": [
+    "javascript",
+    "javascriptreact",
+    "html",
+    "vue"
+  ]
+}
